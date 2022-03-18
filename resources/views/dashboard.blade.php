abf73e506bbcc9daef357715401c1e317f0b54a0
vidalmamanijordan <vidal_mamani@upeu.edu.pe>
2022-03-18 18:48:17 -0500

    Modificando la plantilla Jestream

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Vidal Mamani Jordan
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat beatae labore id provident atque possimus nobis, illum obcaecati voluptatum ratione reiciendis veniam fugiat similique dolorem dolore. Laudantium minima quod obcaecati?</p>
+                <p>Neque beatae unde tempore dicta ipsum, vitae suscipit autem natus perferendis, earum ullam numquam voluptatibus totam, voluptas eos vel eum impedit explicabo dolores voluptates aspernatur quas cum pariatur delectus! Quod.</p>
+                <p>Corrupti minima laboriosam eos molestiae repellendus itaque, velit cupiditate nulla, facilis illum consequatur id architecto ipsum iusto asperiores porro? Temporibus deleniti iusto praesentium at, dolores voluptates nulla! Necessitatibus, mollitia eum?</p>
+                <p>Perferendis culpa omnis ipsa dolor tempora sint. Optio minima quisquam rem esse, voluptatibus odio atque culpa nihil consectetur, sit vero adipisci repellat provident magnam quidem ea dignissimos quis hic? Possimus.</p>
+                <p>Necessitatibus sit temporibus similique fugit, quae, nisi reprehenderit dignissimos qui amet eius nam veritatis molestias in optio. Eos asperiores officiis at nisi obcaecati blanditiis magni vero dolorem aspernatur, enim ab.</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
